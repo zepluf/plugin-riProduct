782cd39c731dca43afa04fe9ddf3f834dbe8ce32
yellow1912 <yellow1912@gmail.com>
2012-06-27 15:01:50 +0700

    minor updates

--- lib/Products.php
+++ lib/Products.php
@@ -47,7 +47,7 @@ class Products extends Object{
 	    return $this->generateCollection($result);
 	}
 	
-	public function findSql($filters = array(), $limit = 0){
+	public function generateSql($filters = array(), $limit = 0){
 	    $sql = "select distinct *
                from " . TABLE_PRODUCTS . " p
                left join " . TABLE_MANUFACTURERS . " m on p.manufacturers_id = m.manufacturers_id
@@ -59,9 +59,33 @@ class Products extends Object{
                and p.products_status = 1 and f.status = 1
                and pd.language_id = '" . (int)$_SESSION['languages_id'] . "'";
 	    
-	    if($limit = 0) $sql .= " limit " . (int)$limit;
+	    if($limit > 0) $sql .= " limit " . (int)$limit;
 	    
 	    return $sql;
+	}
+	
+	/**
+	 * 
+	 * Special note: this function does not return the product object.
+	 * Things may change later
+	 * @param string $listing_split
+	 */
+    public function findBySql($sql){
+        global $db;
+	    $products = array();
+	    
+	    $products_result = $db->Execute($sql);
+        while (!$products_result->EOF) {                    
+            $products[] = array_merge($products_result->fields,
+                array(
+        			'products_display_price' => zen_get_products_display_price($products_result->fields['products_id']),    			            			
+        			'products_link' => zen_href_link(zen_get_info_page($products_result->fields['products_id']), 'products_id=' . $products_result->fields['products_id'])                        
+                )
+            );            
+            $products_result->MoveNext();
+        }
+	    
+        return $products;
 	}
 	
 	private function generateCollection($result){	   
